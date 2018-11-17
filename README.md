ccf2dc22fdd64c9a5928bc74f76e0dd5c1c0aff5
wkxprojet <31353wang@sina.com>
2018-11-17 16:16:00 +0800

    暂存更改

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Efficient_mobile
 web移动端高效开发
+
+* 移动web前端高效开发实践
+    * chapter02 移动web开发环境搭建
---
+++ chapter02/replace_rule.js
@@ -0,0 +1,6 @@
+module.exports=[
+    { // 单文件替换(在线)
+        pattern:'index.js', //匹配替换的单个文件
+        responder:'http://www.kaixiang.com/assets/js/index.js' //
+    }
+]
